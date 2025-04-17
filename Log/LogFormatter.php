eb3e9277ca9a368d874b056e020c6307704d5d67
Piotr Matras <piotr.matras@creativestyle.pl>
2025-04-17 10:30:35 +0200

    feat: MGS-5450 fix issue with log formatter

--- Log/LogFormatter.php
+++ Log/LogFormatter.php
@@ -7,14 +7,23 @@ class LogFormatter implements \Monolog\Formatter\FormatterInterface
     public function format(\Monolog\LogRecord $record)
     {
         return sprintf('[%s] [%s] %s',
-            date('Y-m-d H:i:s', $record[0]),
-            $record[1],
-            $record[2]
+            $record->datetime->format('Y-m-d H:i:s'),
+            $record->level->getName(),
+            $record->message
         );
     }
 
     public function formatBatch(array $records)
     {
+        foreach ($records as $key => $record) {
+            $records[$key] = new \Monolog\LogRecord(
+                message: $record[2],
+                level: \Monolog\Level::fromName($record[1]),
+                channel: 'monolog',
+                datetime: new \Monolog\JsonSerializableDateTimeImmutable(true),
+            );
+        }
+
         return implode("\n", array_map([$this, 'format'], $records));
     }
 }
