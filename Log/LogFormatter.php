ff192eed710e10862359c8aac1e1b147f716d469
Zbigniew Kuraś <zbigniew.kuras@creativestyle.pl>
2026-06-23 12:59:47 +0200

    feat: MGS-5727 revert changes

--- Log/LogFormatter.php
+++ Log/LogFormatter.php
@@ -4,17 +4,8 @@ namespace MageOps\NodeWarmer\Log;
 
 class LogFormatter implements \Monolog\Formatter\FormatterInterface
 {
-    public function format(array|\Monolog\LogRecord $record)
+    public function format(\Monolog\LogRecord $record)
     {
-        if (is_array($record)) {
-            [$timestamp, $level, $message] = $record;
-            return sprintf('[%s] [%s] %s',
-                date('Y-m-d H:i:s', $timestamp),
-                $level,
-                $message
-            );
-        }
-
         return sprintf('[%s] [%s] %s',
             $record->datetime->format('Y-m-d H:i:s'),
             $record->level->getName(),
